172ff1d1357149c93f9b25e1422e990ce191d5c2
w3sl3y18 <wesleyvolk@hotmail.com>
2016-10-24 20:37:46 -0200

    Tentando inserir paginas WEB

---
+++ WebContent/js/validaCadastroGuincho.js
@@ -0,0 +1,104 @@
+function mascaraInteiro(){
+        if (event.keyCode < 48 || event.keyCode > 57){
+                event.returnValue = false;
+                return false;
+        }
+        return true;
+}
+
+//adiciona mascara ao telefone
+function mascaraTelefone(tel){  
+        if(mascaraInteiro(tel)==false){
+                event.returnValue = false;
+        }       
+        return formataCampo(tel, '(00) 0000-0000', event);
+}
+
+//adiciona mascara de cnpj
+function mascaraCNPJ(cnpj){
+        if(mascaraInteiro(cnpj)==false){
+                event.returnValue = false;
+        }       
+        return formataCampo(cnpj, '00.000.000/0000-00', event);
+}
+
+//verificar se o nome esta preenchido
+function validarNome() {
+var nome = form2.empresa.value;
+ 
+if (empresa == "") {
+alert('Preencha o campo com seu nome');
+//form1.nome.focus();
+return false;
+}
+}
+
+function validaTelefone(tel){
+        exp = /\(\d{2}\)\ \d{4}\-\d{4}/
+        if(!exp.test(tel.value))
+                alert('Numero de Telefone Invalido!');
+}
+
+function validaSenha(senha1, senha2){
+
+  if(senha1 != senha2){
+    alert('Senhas diferentes');
+  }
+
+}
+
+//valida o CNPJ digitado
+function validaCNPJ(ObjCnpj){
+        var cnpj = ObjCnpj.value;
+        var valida = new Array(6,5,4,3,2,9,8,7,6,5,4,3,2);
+        var dig1= new Number;
+        var dig2= new Number;
+
+        exp = /\.|\-|\//g
+        cnpj = cnpj.toString().replace( exp, "" ); 
+        var digito = new Number(eval(cnpj.charAt(12)+cnpj.charAt(13)));
+
+        for(i = 0; i<valida.length; i++){
+                dig1 += (i>0? (cnpj.charAt(i-1)*valida[i]):0);  
+                dig2 += cnpj.charAt(i)*valida[i];       
+        }
+        dig1 = (((dig1%11)<2)? 0:(11-(dig1%11)));
+        dig2 = (((dig2%11)<2)? 0:(11-(dig2%11)));
+
+        if(((dig1*10)+dig2) != digito)  
+                alert('CNPJ Invalido!');
+
+}
+
+//formata de forma generica os campos
+function formataCampo(campo, Mascara, evento) { 
+        var boleanoMascara; 
+
+        var Digitato = evento.keyCode;
+        exp = /\-|\.|\/|\(|\)| /g
+        campoSoNumeros = campo.value.toString().replace( exp, "" ); 
+
+        var posicaoCampo = 0;    
+        var NovoValorCampo="";
+        var TamanhoMascara = campoSoNumeros.length;; 
+
+        if (Digitato != 8) { // backspace 
+                for(i=0; i<= TamanhoMascara; i++) { 
+                        boleanoMascara  = ((Mascara.charAt(i) == "-") || (Mascara.charAt(i) == ".")
+                                                                || (Mascara.charAt(i) == "/")) 
+                        boleanoMascara  = boleanoMascara || ((Mascara.charAt(i) == "(") 
+                                                                || (Mascara.charAt(i) == ")") || (Mascara.charAt(i) == " ")) 
+                        if (boleanoMascara) { 
+                                NovoValorCampo += Mascara.charAt(i); 
+                                  TamanhoMascara++;
+                        }else { 
+                                NovoValorCampo += campoSoNumeros.charAt(posicaoCampo); 
+                                posicaoCampo++; 
+                          }              
+                  }      
+                campo.value = NovoValorCampo;
+                  return true; 
+        }else { 
+                return true; 
+        }
+}
